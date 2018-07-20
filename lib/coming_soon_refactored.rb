f553136805f74d7f76d9dfbe51cb4caa134751c8
Cynthia Rittenbach <crittenbach@gmail.com>
2018-07-20 01:44:06 -0400

    fixed require_relatives

--- lib/coming_soon_refactored.rb
+++ lib/coming_soon_refactored.rb
@@ -2,6 +2,5 @@ require "open-uri"
 require "nokogiri"
 require "pry"
 
-require_relative "test_nokogiri/version"
-require_relative "./test_nokogiri/test"
-require_relative "./test_nokogiri/cli"
+require_relative "coming_soon/version"
+require_relative "./coming_soon/cli"
